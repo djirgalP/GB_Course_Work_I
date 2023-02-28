c7f15dc7a83fd5ceccfab4090a5231ef2c670952
Djirgal <djirgal.poshtarova@yandex.ru>
2023-03-01 00:48:24 +0300

    The code is working

--- Task_final/Program.cs
+++ Task_final/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите значения элементов массива через пробел -> ");
+string[] array = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+//Определяем сколько элементов массива <=3
+int counter = 0;
+for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+            counter += 1;
+    }
+//Если таковые есть, то создаем новый массив из элементов длиной <=3 и выводим на печать
+if (counter > 0)
+{       
+    string[] array_new = new string[counter];
+    counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+        {
+            array_new[counter] = array[i];
+            counter += 1;
+        }
+    }
+    Console.Write("[");
+    Console.Write(String.Join(", ", array_new));
+    Console.Write("]");
+} else 
+    Console.WriteLine("[]");
